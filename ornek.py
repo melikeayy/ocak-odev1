e2f3f15e44a3138b805da2c3f55161b5a72f8312
Melike Ay <melikeeay07@gmail.com>
2025-03-01 14:39:39 +0300

    Ocak ödev 2 kodları eklendi

---
+++ ornek.py
@@ -0,0 +1,8 @@
+sayı = int(input("Bir sayı girin:"))
+
+if sayı < 0:
+    print("Girdiğiniz sayı negatif.")
+elif sayı % 2 ==0:
+    print("Girdiğiniz sayı çift.")
+else:
+    print("Girdiğiniz sayı tek.")
